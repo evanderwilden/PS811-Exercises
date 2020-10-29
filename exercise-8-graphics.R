e7055304d662b48f2f79f9d9b6c46773b4bbb9e3
evanderwilden <ethan.vanderwilden@wisc.edu>
2020-10-29 14:02:04 -0500

    Adding ex8, graphics

---
+++ exercise-8-graphics.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+USArrests
+
+
+
+          ####   BASE R   ####
+
+#Scatterplot for Rape arrests vs Murder Arrests
+plot(USArrests$Rape, USArrests$Murder, 
+     main = "Relationship between \n Rape and Murder Arrests",
+     xlab = "Rape Arrests per 100,000", ylab = "Murder Arrests per 100,000")
+
+#Boxplot for Rape Arrests by State
+boxplot(USArrests$Rape, main = "Boxplot for US Rape Arrests by State", 
+        ylab = "Number of Arrests (per 100,000)")
+
+#Barplot for Rape Arrests by State
+barplot(USArrests$Rape, main = "Barplot for US Rape Arrests by State",
+        ylab = "Number of Arrests (per 100,000)")
+
+#Histogram for % Urban Population
+hist(USArrests$UrbanPop, main = "Histogram of % Urban Population",
+     xlab = "Percent Urban Population", prob = TRUE)
+
+
+      ####    GGPLOT    ####
+
+#Scatterplot for Rape arrests vs Murder Arrests
+ggplot(data=USArrests, aes(x=Rape, y = Murder)) +
+  geom_point(col = 10) + geom_smooth(method = "lm") +
+  labs(title = "Relationship between Rape and Murder Arrests", 
+       y = "Murder Arrests(per 100,000)",
+       x= "Rape Arrests (per 100,000)", 
+       subtitle = "By US State", caption = "Data from USArrests in R")
+
+#Boxplot for Rape Arrests by State
+ggplot(data=USArrests, aes(x=Rape))+
+  geom_boxplot(col = 1) + 
+  labs(title = "Boxplot for US Rape Arrests", 
+       subtitle = "By US State", caption = "Data from USArrests in R",
+       x= "Number of Arrests (per 100,000)" )
+
+#Barplot for Rape Arrests by State
+ggplot(data=USArrests, aes(x = row.names(USArrests), y = Rape)) +
+  geom_bar(stat= "Identity") +
+  theme(axis.text.x= element_text(angle=90, hjust=1, size = 5)) +
+  labs(title = "Barplot for US Rape Arrests", 
+       subtitle = "By US State", caption = "Data from USArrests in R",
+       x= "Number of Arrests (per 100,000)" )
+
+
+#Histogram for % Urban Population
+ggplot(data=USArrests, aes(x=UrbanPop)) +
+  geom_histogram(color = "Black", fill = "lightblue", bins = 10)+
+  labs(title = "Histogram For % Urban Population", 
+       subtitle = "By US State", caption = "Data from USArrests in R",
+       x= "Percent Urban Pppulation" )
+  
+
